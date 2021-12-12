7821f91864e55c04d46e8c66e4c11876e05f8b88
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2021-12-12 18:01:59 +0700

    Fix connect DB
    - API test create account --done
    - API test get jwt --done
    - fix connect to MongoDB atlas

--- chovietz/src/main/java/com/chovietz/config/MongoDbConfig.java
+++ chovietz/src/main/java/com/chovietz/config/MongoDbConfig.java
@@ -1,15 +1,22 @@
-package com.chovietz.config;
+//package com.chovietz.config;
+//
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//import com.mongodb.client.MongoDatabase;
+//
+//public class MongoDbConfig {
+//	private String connection = "mongodb+srv://r12:r12@dichothuez.vgf8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//	private String DbName = "dichothuez";
+//	
+//	public MongoDatabase createConnection() {
+//		MongoClient client = MongoClients.create(connection);
+//		return client.getDatabase(DbName);
+//	}
+//}
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-
-public class MongoDbConfig {
-	private String connection = "mongodb+srv://r12:r12@dichothuez.vgf8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-	private String DbName = "dichothuez";
-	
-	public MongoDatabase createConnection() {
-		MongoClient client = MongoClients.create(connection);
-		return client.getDatabase(DbName);
-	}
-}
+//ConnectionString connectionString = new ConnectionString("mongodb+srv://r12:r12@dichothuez.vgf8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+//MongoClientSettings settings = MongoClientSettings.builder()
+//        .applyConnectionString(connectionString)
+//        .build();
+//MongoClient mongoClient = MongoClients.create(settings);
+//MongoDatabase database = mongoClient.getDatabase("test");
