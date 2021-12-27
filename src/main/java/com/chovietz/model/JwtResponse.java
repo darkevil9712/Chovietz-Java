758acb0794027ae45381de704d9b86aa1623489e
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2021-12-28 00:05:00 +0700

    fix conflict CHOV17 with dev

--- src/main/java/com/chovietz/model/JwtResponse.java
+++ src/main/java/com/chovietz/model/JwtResponse.java
@@ -4,12 +4,17 @@ package com.chovietz.model;
 public class JwtResponse {
 
     private final String jwttoken;
+    private String userRole;
 
-    public JwtResponse(String jwttoken) {
+    public JwtResponse(String jwttoken, String userRole) {
         this.jwttoken = jwttoken;
+        this.userRole = userRole;
     }
 
     public String getToken() {
         return this.jwttoken;
+    }
+    public String getUserRole() {
+    	return this.userRole;
     }
 }
